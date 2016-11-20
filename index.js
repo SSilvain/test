2c8431fad495f731cb3dd4a244468c8f9e192bfc
Silvain <silver383@gmail.com>
2016-11-20 09:09:03 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var pug = require("pug");
+var app = express();
+app.set('view engine', 'pug');
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/views/public'));
+
+app.get('/', function(req, res) {
+	res.render("home");
+});
+
+// app.get('/about', function(req, res) {
+// 	res.render("about");
+// });
+
+var fortunes = [
+	"Победи свои страхи, или они победят тебя.",
+	"Рекам нужны истоки.",
+	"Не бойся неведомого.",
+	"Тебя ждет приятный сюрприз.",
+	"Будь проще везде, где только можно.",
+];
+
+app.get('/about', function(req, res) {
+	var randomFortune =
+		fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about', { fortune: randomFortune });
+});
+
+// пользовательская страница 404
+app.use(function(req, res, next) {
+	res.status(404);
+	res.render("404");
+});
+
+// пользовательская страница 505
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.status(500);
+	res.render("500");
+});
+
+app.listen(app.get('port'), function() {
+	console.log('Express запущен на http://localhost:' +
+		app.get('port') + '; нажмите Ctrl+C для завершения.');
+});
